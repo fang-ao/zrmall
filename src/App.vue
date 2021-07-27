7c03a5cfb8a407d89ea9bcd6be0db487075fafff
fangao <1946193035@qq.com>
2021-07-27 17:35:27 +0800

    项目基础骨架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <main-tab-bar/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import MainTabBar from "./components/content/mainTabbar/MainTabBar";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {MainTabBar}
 }
 </script>
 
 <style>
+@import "./assets/css/base.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
